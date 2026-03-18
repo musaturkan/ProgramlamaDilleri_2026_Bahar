d78a39f209cf14df743944be6e5139bf48ff81c4
musaturkan <musa.turkan@bilecik.edu.tr>
2026-03-18 11:08:00 +0300

    aaa

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using DataModel;
+using Microsoft.Extensions.DependencyInjection;
+using Services;
+using Services.Fabrika;
 
 internal class Program
 {
@@ -10,30 +13,49 @@ internal class Program
     }
     private static void Main(string[] args)
     {
-        ///Stack ve Heap Kavramları
-        ///Value Type ve Reference Type Kavramları
-        ///Garbage Collector (Çöp Toplama) Mekanizması
-        ///
-        int sayi1 = 10; //Stack'te saklanır
-        int sayi2 = sayi1; //sayi1'in değeri sayi2'ye kopyalanır
-        sayi2 = 20; //sayi2'nin değeri değiştirilir, sayi1 etkilenmez
-        Console.WriteLine(sayi1);
+        var servisler = new ServiceCollection();
 
-        Urun urun1; //Stack'te saklanır, ancak içindeki veriler Heap'te saklanır
-        urun1 = new Urun { Id = 1, Ad = "Laptop", Fiyat = 5000 }; //Heap'te saklanır
-        //Urun urun1 = new Urun { Id = 1, Ad = "Laptop", Fiyat = 5000 }; //Heap'te saklanır
-        ///urun1.Ad = "Masaüstü Bilgisayar"; //urun1'in Ad özelliği değiştirilir
+        servisler.AddTransient<IUrunService, UrunService>();
+        servisler.AddScoped<IMarkaService, MarkaService>();
+        servisler.AddSingleton<IFabrika, ServiceFabrika>();       
 
-        Urun urun2 = new Urun { Id = 2, Ad = "Telefon", Fiyat = 3000 }; //Heap'te saklanır
+        var serviceProvider = servisler.BuildServiceProvider();
 
-        ///urun2 = urun1; //urun1'in referansı urun2'ye atanır, her iki değişken de aynı nesneyi gösterir
+        var fabrika = serviceProvider.GetService<IFabrika>();
+        //var urunService = serviceProvider.GetService<IUrunService>();
+        var urunService = fabrika.UrunServiceOlustur();
+        urunService.Yazdir(3);
 
-        //urun2.Ad= "Tablet"; //urun2'nin Ad özelliği değiştirilir, urun1 de etkilenir çünkü her iki değişken aynı nesneyi gösterir
+        /////Stack ve Heap Kavramları
+        /////Value Type ve Reference Type Kavramları
+        /////Garbage Collector (Çöp Toplama) Mekanizması
+        /////
+        //int sayi1 = 10; //Stack'te saklanır
+        //int sayi2 = sayi1; //sayi1'in değeri sayi2'ye kopyalanır
+        //sayi2 = 20; //sayi2'nin değeri değiştirilir, sayi1 etkilenmez
+        //Console.WriteLine(sayi1);
 
-        Degistir(urun2);
+        //Urun urun1; //Stack'te saklanır, ancak içindeki veriler Heap'te saklanır
+        //urun1 = new Urun { Id = 1, Ad = "Laptop", Fiyat = 5000 }; //Heap'te saklanır
+        ////Urun urun1 = new Urun { Id = 1, Ad = "Laptop", Fiyat = 5000 }; //Heap'te saklanır
+        /////urun1.Ad = "Masaüstü Bilgisayar"; //urun1'in Ad özelliği değiştirilir
 
-        Console.WriteLine(urun1.Ad); //Laptop
-        Console.WriteLine(urun2.Ad);
+        //Urun urun2 = new Urun { Id = 2, Ad = "Telefon", Fiyat = 3000 }; //Heap'te saklanır
+
+        /////urun2 = urun1; //urun1'in referansı urun2'ye atanır, her iki değişken de aynı nesneyi gösterir
+
+        ////urun2.Ad= "Tablet"; //urun2'nin Ad özelliği değiştirilir, urun1 de etkilenir çünkü her iki değişken aynı nesneyi gösterir
+
+        //Degistir(urun2);
+
+        //Console.WriteLine(urun1.Ad); //Laptop
+        //Console.WriteLine(urun2.Ad);
+
+
+        ////////////Metot çağırma işlemi//////
+        //Services.UrunService urunService = new Services.UrunService();
+        var urunService = Services.ServiceFabrika.UrunServiceOlustur();
+        urunService.Yazdir(3);
 
     }
 
